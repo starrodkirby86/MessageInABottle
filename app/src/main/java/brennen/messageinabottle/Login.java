9984bf5389a454b0fe7b8e85c7931de3df4cb507
BrennenFagan <rydrion@mail.fresnostate.edu>
2015-10-05 20:55:01 -0700

    Trying to revert the push, backing up hopefully

--- app/src/main/java/brennen/messageinabottle/Login.java
+++ app/src/main/java/brennen/messageinabottle/Login.java
@@ -1,11 +1,10 @@
 package brennen.messageinabottle;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -16,6 +15,7 @@ public class Login extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,7 +24,13 @@ public class Login extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Trying to get link to work: http://stackoverflow.com/questions/2734270/how-do-i-make-links-in-a-textview-clickable/2746708#2746708
+        TextView support= (TextView) findViewById(R.id.support);
+        support.setMovementMethod(LinkMovementMethod.getInstance());
     }
+
 
 }
--- app/src/main/java/brennen/messageinabottle/MainActivity.java
+++ app/src/main/java/brennen/messageinabottle/MainActivity.java
@@ -2,8 +2,6 @@ package brennen.messageinabottle;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -19,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
     }
 
     @Override
